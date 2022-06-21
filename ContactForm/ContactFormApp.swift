526dc2e52a6f0e31ce37282b1d57e1838ca595b8
Omar Nava <omar.mac.97@gmail.com>
2022-06-21 10:18:28 -0500

    Initial Commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Ricardo Omar Martinez Nava on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactForm
+//
+//  Created by Ricardo Omar Martinez Nava on 21/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
