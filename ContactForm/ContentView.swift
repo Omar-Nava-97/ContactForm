9566894a7ff7b2ef505bb2558fc11c53686d0fed
Omar Nava <omar.mac.97@gmail.com>
2022-07-04 18:19:12 -0500

    Commit Final

--- ContactForm/ContentView.swift
+++ ContactForm/ContentView.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAddress = ""
+    @State private var phoneNumber = ""
+    @State private var message = "Write your message here"
+    @State private var subject = "Help"
+    @State private var includeLogs = false
+    
+    @State private var preferredContactMethod = ContactMethod.email
+    
+    enum ContactMethod {
+        case email, phone
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+        
+        Form {
+            Section(header: Text ("How can reach you??")) {
+                Picker("Contac Method", selection: $preferredContactMethod) {
+                    Text("Email").tag(ContactMethod.email)
+                    Text("Phone").tag(ContactMethod.phone)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                switch preferredContactMethod {
+                case .email:
+                    TextField("Email", text: $emailAddress)
+                case .phone:
+                    TextField("Phone Number", text: $phoneNumber)
+                }
+        
+            }
+            
+            Section(header: Text("What can we help you with?")) {
+                Picker("Subject", selection: $subject) {
+                    ForEach(["Help", "Suggestion", "Bug Report"], id: \.self) {
+                        subject in Text(subject)
+                    }
+                }
+            }
+            
+            Section(header: Text ("Briefly explain what's going on.")) {
+            TextEditor(text: $message)
+            }
+            
+            Section(footer: Text("This information will be sent anonymously")) {
+            Toggle("Include Logs", isOn: $includeLogs)
+            }
+            Button("submit", action: {
+                print("Submit button tapped")
+                })
+            }
+        .navigationTitle("Contact Us")
+        }
     }
 }
 
